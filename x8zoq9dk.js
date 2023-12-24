f17c346165ef2b0fac40006dbec840cf591b74cd
castile61251511 <154744256+castile61251511@users.noreply.github.com>
2023-12-24 22:20:46 +0800

    Create x8zoq9dk.js

---
+++ x8zoq9dk.js
@@ -0,0 +1,8 @@
+xiFSqu78xd = function() {
+
+$(document).ready(function() {
+  				$(".ldio-xurctdeofga").fadeOut();
+					var t = document.getElementById('gameContainer');
+					t.style.visibility = 'visible';
+				});
+}
